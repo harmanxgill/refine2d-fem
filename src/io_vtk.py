d6ad4fdd8b5d83d60a131862b0b43ac3feeede85
Harman Gill <gllxharman@gmail.com>
2025-08-08 16:23:10 -0400

    Visualization

---
+++ src/io_vtk.py
@@ -0,0 +1,19 @@
+import meshio
+import numpy as np
+import os
+
+def write_vtu(coords, tris, point_data=None, cell_data=None, path="out/solution.vtu"):
+    """
+    Write a VTU file.
+    Args:
+        coords: (N,2)
+        tris: (M,3)
+        point_data: dict of (N,1) arrays
+        cell_data: dict of (M,1) arrays
+    """
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    cells = [ ("triangle", tris.astype(np.int32)) ]
+    pd = {} if point_data is None else point_data
+    cd = {} if cell_data is None else cell_data
+    mesh = meshio.Mesh(points=coords.astype(float), cells=cells, point_data=pd, cell_data=cd)
+    mesh.write(path)
